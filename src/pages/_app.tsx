1950c53369b2c0b0160c6c518e6e7d080ced63d3
Wilker Viana <wilker.info@gmail.com>
2021-02-28 18:55:59 -0300

    fix: :mag: fix image tag metatags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         />
         <meta
           property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          content="/assets/images/thumbnail.jpg"
         />
 
         <meta property="twitter:card" content="summary_large_image" />
@@ -33,7 +33,7 @@ function MyApp({ Component, pageProps }) {
         />
         <meta
           property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          content="/assets/images/thumbnail.jpg"
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
